a30ca5bd68fc58ee5ea6746caebda437c09363cb
gideongannaban <gideongannaban1985@gmail.com>
2020-08-06 23:29:58 +0000

    Initial commit

---
+++ assets/js/countrycases.js
@@ -0,0 +1,302 @@
+// Leaflet JS GeoJason Markers 
+
+var myMap = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.82,53.0853 ]
+    },
+    "properties": {
+    "County":"Carlow",
+    "Cases":399
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.248889,53.333056 ]
+    },
+    "properties": {
+    "County":"Dublin",
+    "Cases":12609
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.4575,52.334167 ]
+    },
+    "properties": {
+    "County":"Wexford",
+    "Cases":224
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.049444,52.975 ]
+    },
+    "properties": {
+    "County":"Wicklow",
+    "Cases":712
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.3,53.032222 ]
+    },
+    "properties": {
+    "County":"Laois",
+    "Cases":370
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.488889,53.273889 ]
+    },
+    "properties": {
+    "County":"Offaly",
+    "Cases":638
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.416667,54 ]
+    },
+    "properties": {
+    "County":"Louth",
+    "Cases":795
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.749997,53.166666 ]
+    },
+    "properties": {
+    "County":"Kildare",
+    "Cases":1651
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.791667,53.555 ]
+    },
+    "properties": {
+    "County":"Meath",
+    "Cases":831
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.499998,53.499998 ]
+    },
+    "properties": {
+    "County":"Westmeath",
+    "Cases":767
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.252222,52.654167 ]
+    },
+    "properties": {
+    "County":"Kilkenny",
+    "Cases":360
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.8,53.733333 ]
+    },
+    "properties": {
+    "County":"Longford",
+    "Cases":705
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -9,52.83333 ]
+    },
+    "properties": {
+    "County":"Clare",
+    "Cases":399
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -8.495833,51.898611 ]
+    },
+    "properties": {
+    "County":"Cork",
+    "Cases":1570
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -9.749997,52.166666 ]
+    },
+    "properties": {
+    "County":"Kerry",
+    "Cases":318
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -8.623056,52.664722 ]
+    },
+    "properties": {
+    "County":"Limerick",
+    "Cases":610
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.703889,52.355 ]
+    },
+    "properties": {
+    "County":"Tipperary",
+    "Cases":545
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.111944,52.258333 ]
+    },
+    "properties": {
+    "County":"Waterford",
+    "Cases":166
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -9.048889,53.271944 ]
+    },
+    "properties": {
+    "County":"Galway",
+    "Cases":498
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -8.002,54.1166662 ]
+    },
+    "properties": {
+    "County":"Leitrim",
+    "Cases":265
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -9.249999,53.899996 ]
+    },
+    "properties": {
+    "County":"Mayo",
+    "Cases":584
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -8.183333,53.633333 ]
+    },
+    "properties": {
+    "County":"Roscommon",
+    "Cases":536
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -8.483333,54.266667 ]
+    },
+    "properties": {
+    "County":"Sligo",
+    "Cases":229
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -7.360556,53.990833 ]
+    },
+    "properties": {
+    "County":"Cavan",
+    "Cases":1150
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -8.116667,54.65 ]
+    },
+    "properties": {
+    "County":"Donegal",
+    "Cases":476
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -6.966667,54.25 ]
+    },
+    "properties": {
+    "County":"Monaghan",
+    "Cases":883
+    }
+  }
+]
+}
+L.geoJSON(myMap, {
+
+    onEachFeature : countyInfo
+
+}).addTo(map);
+
+function countyInfo (feature, layer) {
+    layer.bindPopup("<p>County Name:" + feature.properties.County + "<p>Number of Cases:" + feature.properties.Cases);    
+}
---
+++ assets/js/map.js
@@ -0,0 +1,26 @@
+
+ 
+var mapTileLayers = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
+});
+ 
+var map = L.map("map", {
+    layers: [mapTileLayers],
+    center: [53.350140, -6.266155],
+    zoom: 5
+});
+
+var myMarker = L.marker([53.350140, -6.266155]).addTo(map);  
+ 
+ 
+// 
+// 1. When I click on the county, the marker on the map will change 
+    
+
+$("#county").on("change", function () {
+            var countyData = $(this).val().split(","),
+                MapLat = countyData[0],
+                MapLng = countyData[1],
+                MapZoom = parseInt(countyData[2]);
+            map.flyTo([MapLat, MapLng], MapZoom);
+        });
