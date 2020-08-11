472a5a37d1886fd9314b5fd2961a82e46c764dfb
gideongannaban <gideongannaban1985@gmail.com>
2020-08-11 23:39:22 +0000

    Updated ID handler and change format of country.js data

--- assets/js/country.js
+++ assets/js/country.js
@@ -1,27 +1,33 @@
+window.onload = function () {
+    getCovidData();
+};
 
-  $(document).ready(function(){
-    init()
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
 
+function getCovidData() {
+  fetch("https://api.covid19api.com/summary", requestOptions)
+    .then(function(resp) {return resp.json()})
+    .then(function(data) {
+        let totalCase = data.Global.TotalConfirmed;
+        let totalRecovery = data.Global.TotalRecovered;
+        let totalDeath = data.Global.TotalDeaths;
+        let dailyCase = data.Global.NewConfirmed;
+        let dailyRecovery = data.Global.NewRecovered;
+        let dailyDeath = data.Global.NewDeaths;
 
-    function init(){
-        var url = "https://api.covid19api.com/summary"
+        document.getElementById('global-case').innerHTML = totalCase.toLocaleString('en');
+        document.getElementById('global-recovery').innerHTML = totalRecovery.toLocaleString('en');
+        document.getElementById('global-death').innerHTML = totalDeath.toLocaleString('en');
+        document.getElementById('global-daily-cases').innerHTML = dailyCase.toLocaleString('en');
+        document.getElementById('global-daily-recovery').innerHTML = dailyRecovery.toLocaleString('en');
+        document.getElementById('global-daily-deaths').innerHTML = dailyDeath.toLocaleString('en');
 
-        var worldwide = [];
-       
-
-
-
-       $.get(url, function(worldwide){
-           console.log(worldwide.Global)
-
-        worldwide = `
-                ${worldwide.Global.TotalConfirmed}                
-            `                     
- 
-            $("#worldwide").html(worldwide)
-           
-           
-        })
-    }
-  })
-
+    })
+    .catch(function(){
+        console.log("error");
+    }) 
+    // setTimeout(getCovidData, 43,200,000) // this will update every 12 hrs    
+}
